46f1ee0ab6daa96faf1221b541455122fb02edd7
Rizky Alfiansyah <rizkkyaf@gmail.com>
2023-04-28 15:33:28 +0800

    fix : add route to header

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import instance from '@/lib/axios';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'News App',
@@ -14,9 +15,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="relative flex min-h-screen flex-col items-center justify-start lg:gap-4 gap-3 bg-slate-100 overflow-x-hidden">
         <header className="w-full flex xl:justify-center justify-start items-center py-4 shadow-md bg-white">
-          <h1 className="xl:w-[1280px] lg:text-4xl text-2xl font-bold text-blue-900 px-2">
-            NEWS Updates
-          </h1>
+          <Link href="/">
+            <h1 className="xl:w-[1280px] lg:text-4xl text-2xl font-bold text-blue-900 px-2">
+              NEWS Updates
+            </h1>
+          </Link>
         </header>
         {children}
       </body>
